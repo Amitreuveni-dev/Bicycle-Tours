b820b611aa5bc8eef6862fe81832a2373851aa5a
eeitan <eeitan12@gmail.com>
2026-05-04 11:20:59 +0300

    ss

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -52,7 +52,7 @@ export default function Hero() {
         {/* Badge */}
         <motion.div className={styles.badge} variants={fadeUp}>
           <Compass size={15} aria-hidden="true" />
-          <span>טיולי אופניים בוטיק בארץ ובעולם</span>
+          <span>טיולי בוטיק בארץ ובעולם</span>
         </motion.div>
 
         {/* Headline */}
